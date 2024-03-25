6016b22788ee7aba72bcb9cc7a3cf860aac90d52
ptecc208 <ptecc@naver.com>
2024-03-25 16:05:50 +0900

    test:240325_practice

---
+++ Drill_Algorithm/src/main/java/com/practice/algo/application1.java
@@ -0,0 +1,44 @@
+package com.practice.algo;
+
+public class application1 {
+
+    public static void main(String[] args) {
+
+        //나열된 수에서 최소값과 최댓값 구하기
+
+//        int[] numArray = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
+//
+//        int min = numArray[0];
+//        int max = numArray[0];
+//        int minPos = 0;
+//        int maxPos = 0;
+//
+//        for (int i = 1; i < numArray.length; i++) {
+//            if (min > numArray[i]) {
+//                min = numArray[i];
+//                minPos = i + 1;
+//            } if (max < numArray[i]) {
+//                max = numArray[i];
+//                maxPos = i + 1;
+//            }
+//        }
+//        System.out.println("가장 작은 값은 : " + min + ", 위치는 : " + minPos + "번째 입니다.");
+//        System.out.println("가장 큰 값은 : " + max + ", 위치는 : " + maxPos + "번째 입니다.");
+
+        //Java로 문제를 풀면 일반적으로 다음과 같이 입력을 받고 이후의 로직을 작성.
+
+        /*
+        public class ******* {
+        private static int N, M;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            }
+        }
+         */
+    }
+}
